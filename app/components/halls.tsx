d9f35ea085be5f727da802c63fac7551012eff1f
Jayden Tan <jaydentan@169-231-88-34.wireless.ucsb.edu>
2025-01-11 15:02:17 -0800

    halls

--- app/components/halls.tsx
+++ app/components/halls.tsx
@@ -3,16 +3,16 @@ import HallCard from "./hallcard/hallcard";
 export default function Halls() {
   return (
     <div className="flex space-x-4">
-      <div className="text-white rounded w-[25vw]">
+      <div className="text-white rounded w-[23vw]">
         <HallCard name="Carrillo" picture="/carrillo-pizza-oven.jpeg" text="..." />
       </div>
-      <div className="text-white rounded w-[25vw]">
+      <div className="text-white rounded w-[23vw]">
         <HallCard name="De La Guerra" picture="/DLG.jpeg" text="..." />
       </div>
-      <div className="text-white rounded w-[25vw]">
+      <div className="text-white rounded w-[23vw]">
         <HallCard name="Ortega" picture="/ortega.jpeg" text="..." />
       </div>
-      <div className="text-white rounded w-[25vw]">
+      <div className="text-white rounded w-[23vw]">
         <HallCard name="Portola" picture="/portola.jpeg" text="..." />
       </div>
     </div>
