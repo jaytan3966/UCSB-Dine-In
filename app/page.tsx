26d5a7f171bfbbf11f1b7d72788fce7c1565d802
Jayden Tan <jaydentan@169-231-88-34.wireless.ucsb.edu>
2025-01-11 11:05:07 -0800

    updated navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,17 @@ import { Navbar } from "./components/navbar";
 
 export default function Home() {
   const topRef = useRef<HTMLDivElement | null>(null);
-  const reviewRef = useRef<HTMLDivElement | null>(null);
-  const menuRef = useRef<HTMLDivElement | null>(null);
+  const diningRef = useRef<HTMLDivElement | null>(null);
   const aboutRef = useRef<HTMLDivElement | null>(null);
 
   return (
     <>
       <header className="sticky top-0 z-10">
-        <Navbar topRef={topRef} reviewRef={reviewRef} menuRef={menuRef} aboutRef={aboutRef} />
+        <Navbar topRef={topRef} diningRef={diningRef} aboutRef={aboutRef} />
       </header>
 
       <main className="flex flex-col justify-center items-center">
-        <Intro topRef={topRef} reviewRef={reviewRef} menuRef={menuRef} aboutRef={aboutRef}/>
+        <Intro topRef={topRef} diningRef={diningRef} aboutRef={aboutRef}/>
         <hr className="w-[100vw] border-[#1d2f54]"/>
 
         <h1 className="font-semibold text-2xl p-2 text-[#1d2f54]" ref={topRef}>Highest Rated Foods</h1>
@@ -41,27 +40,7 @@ export default function Home() {
         <h1>hello</h1>
         <h1>hello</h1>
         <h1>hello</h1>
-        <h1 className="font-semibold text-2xl p-2 text-[#1d2f54]" ref={reviewRef}>Reviews</h1>
-        <hr className="w-[22vw] border-[#1d2f54] p-2"/>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1 className="font-semibold text-2xl p-2 text-[#1d2f54]" ref={menuRef}>Today's Menu</h1>
+        <h1 className="font-semibold text-2xl p-2 text-[#1d2f54]" ref={diningRef}>Dining Commons</h1>
         <hr className="w-[22vw] border-[#1d2f54] p-2"/>
         <h1>hello</h1>
         <h1>hello</h1>
