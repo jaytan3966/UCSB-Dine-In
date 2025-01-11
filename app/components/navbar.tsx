914c5c4cfe2c84e85bcf6bf8b75495f46a129a2c
Jayden Tan <jaytan3966@gmail.com>
2025-01-11 11:05:07 -0800

    updated navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,6 @@
-export interface NavbarProps {
+interface NavbarProps {
   topRef: React.RefObject<HTMLDivElement | null>;
-  reviewRef: React.RefObject<HTMLDivElement | null>;
-  menuRef: React.RefObject<HTMLDivElement | null>;
+  diningRef: React.RefObject<HTMLDivElement | null>;
   aboutRef: React.RefObject<HTMLDivElement | null>;
 }
 
@@ -18,14 +17,13 @@ const scrollHome = () => {
     window.scrollTo({top:0, behavior: "smooth",});
 }
 
-export const Navbar = ({topRef, reviewRef, menuRef, aboutRef} : NavbarProps) => {
+export const Navbar = ({topRef, diningRef, aboutRef} : NavbarProps) => {
     return (
         <div className="bg-[#1d2f54] p-4 flex justify-between items-center max-w-screen">
             <button className="font-bold text-3xl text-[#ffce34]" onClick={() => scrollHome()}>UCSB Dine-In</button>
             <div className="flex justify-end">
             <button className="font-bold text-1xl text-white px-4 hover:-translate-y-1 delay-150 duration-300" onClick={() => handleScrollDown(topRef)}>Highest Rated Food</button>
-            <button className="font-bold text-1xl text-white px-4 hover:-translate-y-1 delay-150 duration-300" onClick={() => handleScrollDown(reviewRef)}>Reviews</button>
-            <button className="font-bold text-1xl text-white px-4 hover:-translate-y-1 delay-150 duration-300" onClick={() => handleScrollDown(menuRef)}>Today's Menu</button>
+            <button className="font-bold text-1xl text-white px-4 hover:-translate-y-1 delay-150 duration-300" onClick={() => handleScrollDown(diningRef)}>Dining Commons</button>
             <button className="font-bold text-1xl text-white px-4 hover:-translate-y-1 delay-150 duration-300" onClick={() => handleScrollDown(aboutRef)}>About</button>
             </div>
         </div>
