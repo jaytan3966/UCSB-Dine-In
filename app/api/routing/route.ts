517064432d768ad3884f76a3d146c4f845fe7f99
Jordan Junaidi <jordanrjunaidi@gmail.com>
2025-01-11 12:51:34 -0800

    Add POST command

--- app/api/routing/route.ts
+++ app/api/routing/route.ts
@@ -1,9 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
-export async function GET(req: NextRequest){
-    const supabase = await createClient();
-    const {data: picks} = await supabase.from("pickdisplay").select();
+export async function POST(req: NextRequest) {
+  const body = await req.json();
 
-    return NextResponse.json(picks);
+  const supabase = await createClient();
+  const { data: reviews } = await supabase.from("reviews").insert(body);
+
+  return NextResponse.json(reviews);
+}
+
+export async function GET(req: NextRequest) {
+  const supabase = await createClient();
+  const { data: reviews } = await supabase.from("reviews").select();
+
+  return NextResponse.json(reviews);
 }
