de203fa28505ef381291d97256329a10b99fc57e
Jordan Junaidi <jordanrjunaidi@gmail.com>
2025-01-11 15:52:51 -0800

    Show menus for dining halls

--- app/hall/[hall]/page.tsx
+++ app/hall/[hall]/page.tsx
@@ -1,9 +1,30 @@
 "use client";
 
+import MenuPage from "../../menu/page";
+
 import { useParams } from "next/navigation";
+
+interface Params {
+  hall: string;
+}
 
 export default function Hall() {
   const params = useParams();
 
-  return <p>{params.hall}</p>;
+  const hall =
+    typeof params.hall === "string"
+      ? params.hall
+      : Array.isArray(params.hall)
+        ? params.hall[0]
+        : undefined;
+
+  console.log(hall?.toLowerCase());
+  if (!hall) {
+    return <div>Error: Invalid Hall</div>;
+  }
+
+  if (hall === "De%20La%20Guerra") {
+    return <MenuPage diningCommonCode="de-la-guerra" />;
+  }
+  return <MenuPage diningCommonCode={hall.toLowerCase()} />;
 }
