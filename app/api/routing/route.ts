cf9651cddd1b00adc92745d764037dc30210892a
Jayden Tan <jaydentan@169-231-88-34.wireless.ucsb.edu>
2025-01-11 12:53:34 -0800

    added reviews card

--- app/api/routing/route.ts
+++ app/api/routing/route.ts
@@ -3,7 +3,7 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function GET(req: NextRequest){
     const supabase = await createClient();
-    const {data: picks} = await supabase.from("pickdisplay").select();
+    const {data: reviews} = await supabase.from("reviews").select();
 
-    return NextResponse.json(picks);
+    return NextResponse.json(reviews);
 }
