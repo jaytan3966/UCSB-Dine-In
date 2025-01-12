98c772024b417bfff999da34be41fb5abbe9fe98
Jayden Tan <jaydentan@169-231-88-34.wireless.ucsb.edu>
2025-01-12 00:08:50 -0800

    fixed deleted content

--- app/components/hallcard/hallcard.tsx
+++ app/components/hallcard/hallcard.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "./HallCard.module.css";
+import styles from "./hallcard.module.css";
 
 interface Hall {
   name: string;
@@ -10,7 +10,7 @@ interface Hall {
 export default function HallCard({ name, picture, text }: Hall) {
   return (
     <Link href={`/hall/${name}`} className="text-black no-underline">
-      <div className={styles.card}>
+      <div className={styles.card} >
         <img className={styles["card-image"]} src={picture} alt={name}></img>
         <h2 className={styles["card-title"]}>{name}</h2>
         <p className={styles["card-text"]}>{text}</p>
