7a68eb9aefde16414c69613feeef7e3fe641babb
Jordan Junaidi <jordanrjunaidi@gmail.com>
2025-01-11 20:17:29 -0800

    Upload works

--- app/specificfood/[specificfood]/page.tsx
+++ app/specificfood/[specificfood]/page.tsx
@@ -3,6 +3,7 @@
 import { useParams } from "next/navigation";
 import ReviewCard from "@/app/components/Review/Review";
 import { useStarsState } from "@/app/components/Review/Stars/useStarsState";
+import { exportedHall } from "@/app/hall/[hall]/page";
 
 interface Params {
   specificFood: string;
@@ -21,5 +22,11 @@ export default function specificFood() {
   console.log(specificFood);
   console.log(filledStars);
 
-  return <ReviewCard food={specificFood} diningHall="test" />;
+  return (
+    <ReviewCard
+      food={specificFood}
+      rating={filledStars}
+      diningHall={exportedHall}
+    />
+  );
 }
