39e3efc85684103c3127a7f3ae7e9d519b72c940
Jayden Tan <jaytan3966@gmail.com>
2025-01-11 18:42:03 -0800

    revised reviewCard

--- app/components/reviewsGrid.tsx
+++ app/components/reviewsGrid.tsx
@@ -9,16 +9,8 @@ export interface ReviewProps {
   diningcommon: string;
 }
 
-<<<<<<< HEAD
-export default function Reviews({ reviews }: { reviews: ReviewProps[] }) {
-  const handleReview = (review: ReviewProps) => {
-    console.log(review);
-  };
-
-=======
 export default function Reviews({reviews}: {reviews: ReviewProps[]}) {
   
->>>>>>> c1a3517ce2496cc3920e679e7681427db70be813
   return (
     <div className="grid grid-cols-5">
       {reviews?.map((review) => (
