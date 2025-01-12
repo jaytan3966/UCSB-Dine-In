09ac188a978177aa5c5b07fe02896b4d889898d3
Jayden Tan <jaytan3966@gmail.com>
2025-01-11 17:14:05 -0800

    finished about us

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {images: {
-  domains: ['www.foodiesfeed.com'], 
+  domains: ['www.foodiesfeed.com', 'media2.nekropole.info'], 
 }};
 
 export default nextConfig;
